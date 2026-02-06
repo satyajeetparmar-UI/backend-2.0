4ef274059a483b8924a7197932a25b8ae1f41404
satyajeet <satyajeet.parmar8@gmail.com>
2026-02-06 14:48:15 +0530

    backend part done with api

--- day9/Backend/src/app.js
+++ day9/Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 
 /**
- * - POST /api/notes
+ * - POST /notes
  * - create new note and save data in mongodb
  * - req.body (title, description)
  */
@@ -26,6 +26,50 @@ app.post('/notes', async (req, res) => {
 })
 
 
+/**
+ * - GET /notes
+ * - Fetch all the notes data from mongodb and send them in response
+ */
+app.get('/notes', async (req, res) => {
+  const notes = await noteModel.find()
+
+  res.status(200).json({
+    message: "Notes fetched successfully",
+    notes
+  })
+})
+
+/**
+ * - DELETE /notes/:id
+ * - Delete notes with the id from req.params
+ */
+app.delete('/notes/:id',async (req, res) => {
+  const id = req.params.id
+  
+  await noteModel.findByIdAndDelete(id)
+
+  res.status(200).json({
+    message: "Note deleted successfully"
+  })
+})
+
+
+/**
+ * - PATCH /notes/:id
+ * - update the description of note
+ * - req.body = {description}
+ */
+app.patch('/notes/:id', async (req, res) => {
+  const id = req.params.id
+
+  const { description } = req.body
+  
+  await noteModel.findByIdAndUpdate(id, {description})
+
+  res.status(200).json({
+    message: "Note updated successfully"
+  })
+})
 
 
 module.exports = app
