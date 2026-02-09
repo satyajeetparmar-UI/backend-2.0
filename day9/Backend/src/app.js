ed31ca0b8274f0cff723149af70f109227ba4804
satyajeet <satyajeet.parmar8@gmail.com>
2026-02-09 13:38:02 +0530

    connect frontend and backend on single server 3000.

--- day9/Backend/src/app.js
+++ day9/Backend/src/app.js
@@ -3,10 +3,13 @@
 const express = require("express");
 const noteModel = require('./models/note.models')
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static('./public'))
 
 /**
  * - POST /notes
@@ -72,5 +75,10 @@ app.patch('/notes/:id', async (req, res) => {
   })
 })
 
+
+app.use('*name', (req, res) => {
+  
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"))
+})
 
 module.exports = app
