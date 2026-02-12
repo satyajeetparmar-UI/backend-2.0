a24bfaf4fcfdeac24704c65e3caa79ac50050dd0
satyajeet <satyajeet.parmar8@gmail.com>
2026-02-12 18:20:29 +0530

    day-13 login api explain and executed.

--- day12/src/routes/auth.routes.js
+++ day12/src/routes/auth.routes.js
@@ -42,4 +42,43 @@ authRouter.post("/register", async (req, res) => {
   })
 })
 
+
+
+/**
+ * login api 
+ */
+
+authRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body
+  
+  const user = await userModel.findOne({ email })
+  
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found with this email address"
+    })
+  }
+
+  const isPasswordCorrect = user.password === password
+
+  if (!isPasswordCorrect) {
+    return res.status(401).json({
+      message: "Wrong password"
+    })
+  }
+
+  const token = jwt.sign({
+    id: user._id,
+  },
+  process.env.JWT_SECRET
+  )
+
+  res.cookie("jwt_token", jwt)
+
+  res.status(200).json({
+    message: "User logged in", 
+    user
+  })
+})
+
 module.exports = authRouter
