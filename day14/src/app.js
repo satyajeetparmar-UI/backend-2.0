dca52cb34c9ac8da38d97457a211036658345206
satyajeet <satyajeet.parmar8@gmail.com>
2026-03-06 18:37:48 +0530

    connecting backend to frontend with cors, axios etc.

--- day14/src/app.js
+++ day14/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({
+  credentials: true, // iski wajah se backend ka server frontend pr cookies ko setup kr sake 
+  origin: "http://localhost:5173" // frontend ka address add krna hai ye change bhi ho sakta hai according to port
+}))
 
 /**
  * require routes
